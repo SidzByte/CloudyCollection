4cf04f5fd56c9a0d141f12878099742aef67c916
Siddharth Singh <134775746+SidzByte@users.noreply.github.com>
2024-11-25 00:02:41 +0530

    Add files via upload

---
+++ crud-app/src/App.js
@@ -0,0 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import AddCategory from './component/AddCategory';
+import Category from './component/Category';
+import RootLayout from './component/RootLayout';
+import Detail from './component/Detail';
+import Update from './component/Update';
+import Login from './component/Login.js';
+import Signup from "./component/Signup";
+import { isLogin } from '../src/util/checkAuth'
+
+const router = createBrowserRouter([
+  { path: "login", element: <Login /> },
+  { path: "signup", element: <Signup /> },
+  {
+    path: "dashboard", loader: () => isLogin, element: <RootLayout />, children: [
+      { path: "", element: <Category /> },
+      { path: "category", element: <Category /> },
+      { path: "add-category", element: <AddCategory /> },
+      { path: "detail/:id", element: <Detail /> }, // Matches /dashboard/detail/:id
+      { path: "edit/:id", element: <Update /> }
+    ]
+  }
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
+}
+
+export default App;
